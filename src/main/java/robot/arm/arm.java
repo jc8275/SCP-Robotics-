42120421671a9531f08ed38232cb9cbe8cc51e29
Isaiah Park <isaiahp211@nycstudents.net>
2025-02-06 21:52:09 +0000

    subspaces for arm and roller intake added hopefully i think

---
+++ src/main/java/robot/arm/arm.java
@@ -0,0 +1,11 @@
+package robot.arm;
+//some of yall gotta do work ongggg i did everything
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+//this import is the engines from difdrive. probably not the right ones. change later. URGENT.
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import robot.Ports;
+
+public class arm extends SubsystemBase{
+    
+}
---
+++ src/main/java/robot/arm/armConstants.java
@@ -0,0 +1,11 @@
+package robot.arm;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+//this import is the engines from difdrive. probably not the right ones. change later. URGENT.
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import robot.Ports;
+
+public class armConstants extends SubsystemBase{
+    
+}
