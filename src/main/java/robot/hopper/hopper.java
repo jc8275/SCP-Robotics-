b37999e9301f601add9e1ed8b4bffa1c0f5c864d
Arthur Zin <zarthy10111@gmail.com>
2025-02-10 21:27:02 +0000

    i did drive hopper and shooter stuff

--- src/main/java/robot/hopper/hopper.java
+++ src/main/java/robot/hopper/hopper.java
@@ -1,11 +1,11 @@
-package robot.arm;
-//
+package robot.hopper;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
-//this import is the engines from difdrive. probably not the right ones. change later. URGENT.
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import robot.Ports;
 
-public class arm extends SubsystemBase{
+public class hopper {
     
 }
