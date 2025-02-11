4b9a9d90397c8adf149a8b0d7dc5879c9a372138
Justin Chen <justinc8275@gmail.com>
2025-02-11 18:25:54 -0500

    Update shooterConstants.java

--- src/main/java/robot/shooter/shooterConstants.java
+++ src/main/java/robot/shooter/shooterConstants.java
@@ -5,21 +5,22 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 public class ShooterConstants {
     
     
-    public static final int SHOOTER_MOTOR_PORT = 1;  
+    public static final int SHOOTER_MOTOR_PORT = 0;  
 
     
-    public static final MotorType SHOOTER_MOTOR_TYPE = 
+    public static final MotorType SHOOTER_MOTOR_TYPE = 0; 
 
    
     public static final double SHOOTER_IDLE_SPEED = 0.0;  
-    public static final double SHOOTER_OPERATING_SPEED = 0.75;  
+    public static final double SHOOTER_OPERATING_SPEED = 0.0;  
 
 
-    public static final double MAX_SHOOTER_SPEED = 1.0;  
-    public static final double MIN_SHOOTER_SPEED = 0.2;  
+    public static final double MAX_SHOOTER_SPEED = 0.0;  
+    public static final double MIN_SHOOTER_SPEED = 0.0;  
 
     
-    public static final double SHOOTER_WHEEL_DIAMETER = 5.0; 
-    public static final double SHOOTER_WHEEL_RPM = 5000.0;  
+    public static final double SHOOTER_WHEEL_DIAMETER = 0.0; 
+    public static final double SHOOTER_WHEEL_RPM = 0.0;  
 
-   
+    //Please add any other constants; Rememeber since this is a shooter and the numbers here are jsut for changes
+}
