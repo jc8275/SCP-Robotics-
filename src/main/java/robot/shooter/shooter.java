a3d66cc813074e38929c14c2e635e2ef6cb7e0bf
Justin Chen <justinc8275@gmail.com>
2025-02-11 18:14:43 -0500

    Update shooter.java

--- src/main/java/robot/shooter/shooter.java
+++ src/main/java/robot/shooter/shooter.java
@@ -1,9 +1,32 @@
-package robot.shooter;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkLowLevel.MotorType;
+package robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import robot.Ports;
-public class shooter {
-    
+
+public class MotorSubsystem extends SubsystemBase {
+
+
+    private final CANSparkMax motor;
+
+
+    public MotorSubsystem(int canId, MotorType motorType) {
+        motor = new CANSparkMax(canId, motorType);
+    }
+
+  
+    public void setSpeed(double speed) {
+        motor.set(speed);
+    }
+
+ 
+    public void stop() {
+        motor.stopMotor();
+    }
+
+
+    @Override
+    public void periodic() {
+       
+    }
 }
