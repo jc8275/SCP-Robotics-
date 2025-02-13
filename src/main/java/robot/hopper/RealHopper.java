0b9e2a88928600a52b543adee2073daa04c34710
Kenneth James <kennethj72@nycstudents.net>
2025-02-13 16:21:16 -0500

    Create RealHopper.java

---
+++ src/main/java/robot/hopper/RealHopper.java
@@ -0,0 +1,11 @@
+package robot.Realhopper;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import robot.Ports;
+
+public class hopper {
+    
+}
