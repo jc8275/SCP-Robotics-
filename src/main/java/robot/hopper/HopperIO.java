8a9cf63b7e133e04ace6b808510c2221aa066f07
Kenneth James <kennethj72@nycstudents.net>
2025-02-13 16:37:31 -0500

    Update HopperIO.java

--- src/main/java/robot/hopper/HopperIO.java
+++ src/main/java/robot/hopper/HopperIO.java
@@ -6,5 +6,11 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import robot.Ports;
 public interface HopperIO {
+  # Elevator Interface Voltage Setter
+
+public interface Elevator {
+    void setVoltage(int voltage);
+}
+
   
 }
