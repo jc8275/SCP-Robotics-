bc5500a5e25026b29806911024beddeda5bbe866
Justin Chen <justinc8275@gmail.com>
2025-02-13 16:43:27 -0500

    Update NoShooter.java

--- src/main/java/robot/shooter/NoShooter.java
+++ src/main/java/robot/shooter/NoShooter.java
@@ -1,1 +1,19 @@
+package robot.shooter;
 
+/
+public class NoShooter implements ShooterIO {
+    @Override
+    public void setVoltage(double voltage) {
+        // Does nothing
+    }
+
+    @Override
+    public void setPower(double power) {
+        // Does nothing
+    }
+
+    @Override
+    public double getVelocity() {
+        return 0;  // Returns a dummy value
+    }
+}
