74f0d24342c67e7a8f94550274de6a954f213a7e
Kenneth James <kennethj72@nycstudents.net>
2025-02-13 16:17:40 -0500

    Update HopperIO.java

--- src/main/java/robot/hopper/HopperIO.java
+++ src/main/java/robot/hopper/HopperIO.java
@@ -1,1 +1,10 @@
+package robot.hopper;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import robot.Ports;
+public interface Hopper IO {
+  
+}
