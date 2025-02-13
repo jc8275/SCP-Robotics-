29cb9afe05bb0406ac4266606b84909ebebcc7a5
Justin Chen <justinc8275@gmail.com>
2025-02-13 16:41:27 -0500

    Update RealShooter.java

--- src/main/java/robot/shooter/RealShooter.java
+++ src/main/java/robot/shooter/RealShooter.java
@@ -1,1 +1,25 @@
+package robot.shooter;
 
+
+public class RealShooter implements ShooterIO {
+    private final MotorController shooterMotor;  // Replace with actual motor controller class
+
+    public RealShooter() {
+        shooterMotor = new MotorController(ShooterConstants.SHOOTER_MOTOR_ID); // Example motor init
+    }
+
+    @Override
+    public void setVoltage(double voltage) {
+        shooterMotor.setVoltage(voltage);
+    }
+
+    @Override
+    public void setPower(double power) {
+        shooterMotor.set(power);
+    }
+
+    @Override
+    public double getVelocity() {
+        return shooterMotor.getVelocity();  // Replace with actual motor velocity method
+    }
+}
