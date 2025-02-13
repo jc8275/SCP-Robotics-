efc53354911a55bf90d9439ed0b9f34f38dab7ae
Justin Chen <justinc8275@gmail.com>
2025-02-13 16:42:33 -0500

    Update SimShooter.java

--- src/main/java/robot/shooter/SimShooter.java
+++ src/main/java/robot/shooter/SimShooter.java
@@ -1,1 +1,21 @@
+package robot.shooter;
 
+/
+public class SimShooter implements ShooterIO {
+    private double velocity = 0;  
+
+    @Override
+    public void setVoltage(double voltage) {
+        velocity = voltage * 100;  // Simulated velocity calculation
+    }
+
+    @Override
+    public void setPower(double power) {
+        velocity = power * 3000;  // Simulated power-based velocity
+    }
+
+    @Override
+    public double getVelocity() {
+        return velocity;
+    }
+}
