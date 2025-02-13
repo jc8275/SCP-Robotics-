ff8bc89210149bfb4413325b0e521af21bd6292d
Justin Chen <justinc8275@gmail.com>
2025-02-13 16:41:50 -0500

    Update ShooterIO.java

--- src/main/java/robot/shooter/ShooterIO.java
+++ src/main/java/robot/shooter/ShooterIO.java
@@ -1,1 +1,13 @@
+package robot.shooter;
 
+
+public interface ShooterIO {
+    /** Sets the shooter motor voltage. */
+    void setVoltage(double voltage);
+    
+    /** Sets the shooter motor power. */
+    void setPower(double power);
+    
+    /** Gets the current shooter velocity (RPM). */
+    double getVelocity();
+}
