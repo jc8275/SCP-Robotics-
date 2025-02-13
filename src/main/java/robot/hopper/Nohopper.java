0bccf595fd3ce977e976747f54279dc21e34d1da
Kenneth James <kennethj72@nycstudents.net>
2025-02-13 16:22:03 -0500

    Create Nohopper.java

---
+++ src/main/java/robot/hopper/Nohopper.java
@@ -0,0 +1,11 @@
+package robot.hopper;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import robot.Ports;
+
+public class Nohopper {
+    
+}
