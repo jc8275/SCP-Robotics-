c6ea5003748c0d7ae5b6ccd3070df950a12ead1c
Arthur Zin <zarthy10111@gmail.com>
2025-02-27 21:45:31 +0000

    added a somewhat skeleton to roller intake

--- src/main/java/robot/rollerIntake/rollerIntake.java
+++ src/main/java/robot/rollerIntake/rollerIntake.java
@@ -7,6 +7,18 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import robot.Ports;
 
 public class rollerIntake extends SubsystemBase{
-    
+    private final CANSparkMax rollerIntakeMotor;
+
+    public rollerIntake(int portMotor){
+        // intakeMotor = new CANSparkMax(portMotor, MotorType.); (idk the motor type lol)
+    }
+
+    public void startRollerIntake(){
+        rollerIntakeMotor.set(3.141592653);//speed of our motor
+    }
+    public void stopRollerIntake(){
+        rollerIntakeMotor.set(0.0); // stops our motor :thumbs_up:
+    }
+
 }
 
