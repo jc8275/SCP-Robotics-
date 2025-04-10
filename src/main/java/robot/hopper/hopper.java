74f403d95ab1dc31320af016bf14ccaa66fad358
java-marshmallow <64167358+Java-Marshmallow@users.noreply.github.com>
2025-04-10 16:08:58 -0400

    Set up index motor

--- src/main/java/robot/hopper/hopper.java
+++ src/main/java/robot/hopper/hopper.java
@@ -5,7 +5,23 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import robot.Ports;
+import robot.hopper.AConstants;
 
-public class hopper {
+public class hopper extends SubsystemBase{
+    private final CANSparkMax indexMotor;
+
+    public indexMotor() {
+        this.indexMotor = new CANSparkMax(1, MotorType.kBrushed);
+    }
+    
+    public void index(double speed){
+        indexMotor.set(AConstants.ROLLER_INTAKE_SPEED);
+    }
+    public void stop(double speed){
+        indexMotor.set(AConstants.ROLLER_INTAKE_STOP);
+    }
+    public void reverse(double speed){
+        indexMotor.set(AConstants.ROLLER_INTAKE_REVERSE_SPEED);
+    }
     
 }
