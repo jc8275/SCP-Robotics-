fc1976dd560275894cef50c6bd8613f40d35a5f6
Kenneth James <kennethj72@nycstudents.net>
2025-04-10 16:06:13 -0400

    Update NoHopper.java

--- src/main/java/robot/hopper/NoHopper.java
+++ src/main/java/robot/hopper/NoHopper.java
@@ -7,8 +7,6 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import robot.Ports;
 
 public class NoHopper {
-   public static final double ELEVATOR_MAX_SPEED = X;
-   public static boolean hasStuff = false;
-   public static final double = ELEVATOR_ACCELERATiON = X;
+   
  
 }
