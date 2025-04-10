f82e1fcf960c8e8881f96b004616eab383fc4b9b
Justin Chen <justinc8275@gmail.com>
2025-04-10 16:28:26 -0400

    Update StorageIO.java

--- src/main/java/robot/storage/StorageIO.java
+++ src/main/java/robot/storage/StorageIO.java
@@ -1,1 +1,7 @@
+package robot.storage;
+
+public interface StorageIO {
+    void setPower(double power);  // Control the storage motor power (positive for intake, negative for eject)
+    double getCurrentLoad();      // Get the current load of items in the storage 
+}
 
