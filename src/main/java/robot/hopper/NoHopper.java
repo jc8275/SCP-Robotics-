d44cbcee852c75cad1205c173a97cfb9a3415625
Kenneth James <kennethj72@nycstudents.net>
2025-04-10 16:20:21 -0400

    Update NoHopper.java

--- src/main/java/robot/hopper/NoHopper.java
+++ src/main/java/robot/hopper/NoHopper.java
@@ -7,6 +7,10 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import robot.Ports;
 
 public class NoHopper {
-   
- 
+   public void setVoltage(double voltage) {
+        elevatorMotor.setVoltage(voltage)
+    }
+    public void getVoltage(double voltage) {
+    return elevatorMotor.getVoltage
+    }
 }
