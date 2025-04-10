3c18f1ef855244ce060df96c81b8f52090885e5c
Kenneth James <kennethj72@nycstudents.net>
2025-04-10 16:22:59 -0400

    Update hopperConstants.java

--- src/main/java/robot/hopper/hopperConstants.java
+++ src/main/java/robot/hopper/hopperConstants.java
@@ -7,7 +7,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import robot.Ports;
 
 public class hopperConstants {
-    public static final double ELEVATOR_MAX_SPEED = X;
+    public static final double ELEVATOR_MAX_SPEED = 50;
    public static boolean hasStuff = false;
-   public static final double = ELEVATOR_ACCELERATiON = X;
+   public static final double ELEVATOR_ACCELERATiON = 2;
+   public static final double ELEVATOR_MAX_DISTANCE = 100.0;
 }
