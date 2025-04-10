d21493b35ab183de3e122563ccf7371a0c704db1
java-marshmallow <64167358+Java-Marshmallow@users.noreply.github.com>
2025-04-10 16:22:14 -0400

    Update hopper.java

--- src/main/java/robot/hopper/hopper.java
+++ src/main/java/robot/hopper/hopper.java
@@ -9,19 +9,4 @@ import robot.hopper.AConstants;
 
 public class hopper extends SubsystemBase{
     private final CANSparkMax indexMotor;
-
-    public indexMotor() {
-        this.indexMotor = new CANSparkMax(1, MotorType.kBrushed);
-    }
-    
-    public void index(double speed){
-        indexMotor.set(AConstants.ROLLER_INTAKE_SPEED);
-    }
-    public void stop(double speed){
-        indexMotor.set(AConstants.ROLLER_INTAKE_STOP);
-    }
-    public void reverse(double speed){
-        indexMotor.set(AConstants.ROLLER_INTAKE_REVERSE_SPEED);
-    }
-    
 }
