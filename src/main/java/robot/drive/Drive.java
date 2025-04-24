c6682edfe66f2146368accc4adc9925f90159814
Akk5250 <Akkumar52505250@gmail.com>
2025-04-24 16:56:44 -0400

    Construction people >:(

--- src/main/java/robot/drive/Drive.java
+++ src/main/java/robot/drive/Drive.java
@@ -3,21 +3,33 @@ package robot.drive;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 
+import static edu.wpi.first.units.Units.Second;
+
 import java.util.List;
 import java.util.function.DoubleSupplier;
 
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.units.Time;
+import edu.wpi.first.units.Unit;
 import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import robot.Constants;
 import robot.Ports;
+import robot.Robot;
+import robot.drive.driveConstants.FF;
+import robot.drive.driveConstants.PID;
 
-public class Drive extends SubsystemBase {
+public class drive extends SubsystemBase {
     private final CANSparkMax leftLeader = new CANSparkMax(Ports.Drive.LEFT_LEADER, MotorType.kBrushless);
     private final CANSparkMax leftFollower = new CANSparkMax(Ports.Drive.LEFT_FOLLOWER, MotorType.kBrushless);
 
@@ -31,7 +43,16 @@ public class Drive extends SubsystemBase {
 
     private final DifferentialDriveOdometry odometry;
 
-    public Drive() {
+    private final SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(FF.kS, FF.kV);
+
+    private final PIDController leftPIDController =
+      new PIDController(PID.kP, PID.kI, PID.kD);
+    private final PIDController rightPIDController =
+      new PIDController(PID.kP, PID.kI, PID.kD);
+
+    private final DifferentialDrivetrainSim driveSim;
+
+    public drive() {
         for (CANSparkMax spark : List.of(leftLeader, leftFollower, rightLeader, rightFollower)) {
             spark.restoreFactoryDefaults();
             spark.setIdleMode(IdleMode.kBrake);
@@ -42,11 +63,11 @@ public class Drive extends SubsystemBase {
 
         leftLeader.setInverted(true);
 
-        leftEncoder.setPositionConversionFactor(DriveConstants.POSITION_FACTOR);
-        rightEncoder.setPositionConversionFactor(DriveConstants.POSITION_FACTOR);
+        leftEncoder.setPositionConversionFactor(driveConstants.POSITION_FACTOR);
+        rightEncoder.setPositionConversionFactor(driveConstants.POSITION_FACTOR);
     
-        leftEncoder.setVelocityConversionFactor(DriveConstants.VELOCITY_FACTOR);
-        rightEncoder.setVelocityConversionFactor(DriveConstants.VELOCITY_FACTOR);
+        leftEncoder.setVelocityConversionFactor(driveConstants.VELOCITY_FACTOR);
+        rightEncoder.setVelocityConversionFactor(driveConstants.VELOCITY_FACTOR);
 
         leftEncoder.setPosition(0);
         rightEncoder.setPosition(0);
@@ -59,11 +80,40 @@ public class Drive extends SubsystemBase {
             rightEncoder.getPosition(), 
             new Pose2d()
         );
+
+        // Simulation
+        driveSim = new DifferentialDrivetrainSim(
+            DCMotor.getMiniCIM(2),
+            driveConstants.GEARING,
+            driveConstants.MOI,
+            driveConstants.DRIVE_MASS,
+            driveConstants.WHEEL_RADIUS,
+            driveConstants.TRACK_WIDTH,
+            driveConstants.STD_DEVS);
     }
 
     private void drive(double leftSpeed, double rightSpeed) {
         leftLeader.set(leftSpeed);
         rightLeader.set(rightSpeed);
+
+        final double realLeftSpeed = leftSpeed * driveConstants.MAX_SPEED;
+        final double realRightSpeed = rightSpeed * driveConstants.MAX_SPEED;
+        
+        final double leftFeedforward = feedforward.calculate(realLeftSpeed);
+        final double rightFeedforward = feedforward.calculate(realRightSpeed);
+    
+        final double leftPID = 
+        leftPIDController.calculate(leftEncoder.getVelocity(), realLeftSpeed);
+        final double rightPID = 
+        rightPIDController.calculate(rightEncoder.getVelocity(), realRightSpeed);
+
+        double leftVoltage = leftPID + leftFeedforward;
+        double rightVoltage = rightPID + rightFeedforward;
+  
+        leftLeader.setVoltage(leftVoltage);
+        rightLeader.setVoltage(rightVoltage);
+
+        driveSim.setInputs(leftVoltage, rightVoltage);
     }
 
     public Command drive(DoubleSupplier vLeft, DoubleSupplier vRight){
@@ -82,7 +132,15 @@ public class Drive extends SubsystemBase {
 
     @Override 
     public void periodic() {
-        updateOdometry(gyro.getRotation2d());
+        updateOdometry(Robot.isReal() ? gyro.getRotation2d() :  driveSim.getHeading());
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        // sim.update() tells the simulation how much time has passed
+        driveSim.update(Constants.PERIOD.in(Second));
+        leftEncoder.setPosition(driveSim.getLeftPositionMeters());
+        rightEncoder.setPosition(driveSim.getRightPositionMeters());
     }
 
     public Pose2d pose() {
