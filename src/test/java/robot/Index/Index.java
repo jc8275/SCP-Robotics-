baaa8d5adc51ae39c498f123ece295b573075411
Oscar P <oscarisback1908@gmail.com>
2025-05-05 01:10:33 -0400

    Republishing it

---
+++ src/test/java/robot/Index/Index.java
@@ -0,0 +1,57 @@
+package robot.index;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+
+import static edu.wpi.first.units.Units.Second;
+
+import java.util.List;
+import java.util.function.DoubleSupplier;
+
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.units.Time;
+import edu.wpi.first.units.Unit;
+import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import robot.Constants;
+import robot.Ports;
+import robot.Robot;
+
+public class Index extends SubsystemBase {
+    private final CANSparkMax indexMotor;
+
+    public Index() {
+        indexMotor = new CANSparkMax(Ports.INDEX_MOTOR, MotorType.kBrushless);
+        indexMotor.restoreFactoryDefaults();
+        indexMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+        indexMotor.setSmartCurrentLimit(30);
+    }
+
+    public void setSpeed(double speed) {
+        indexMotor.set(speed);
+    }
+
+    public void stop() {
+        indexMotor.set(0);
+    }
+
+    public Command runIndexerCommand() {
+        return run(() -> setSpeed(IndexConstants.INDEXER_MAX_SPEED));
+    }
+    
+
+    public Command stopIndexerCommand() {
+        return run(() -> setSpeed(0));
+    }
+}
+
---
+++ src/test/java/robot/Index/IndexConstants.java
@@ -0,0 +1,7 @@
+package robot.index;
+
+public class IndexConstants {
+    public static final double INDEXER_MAX_SPEED = 0.5; // Maximum speed for the indexer motors
+    public static final double INDEXER_ACCELERATION = 0.2; // Acceleration rate for motors
+    public static final boolean HAS_ITEMS = false; // Flag to indicate if items are present in the indexer
+}
