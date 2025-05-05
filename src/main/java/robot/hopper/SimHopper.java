fa85311321b73bd9b569c926f2ff1a6530b52888
Linze <dacoolboime@gmail.com>
2025-05-04 21:07:52 -0400

    fixed some hopper issues

--- src/main/java/robot/hopper/SimHopper.java
+++ src/main/java/robot/hopper/SimHopper.java
@@ -1,11 +1,5 @@
 package robot.hopper;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkLowLevel.MotorType;
-
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import robot.Ports;
-
 public class SimHopper {
     
 }
--- src/main/java/robot/hopper/hopperIO.java
+++ src/main/java/robot/hopper/hopperIO.java
@@ -1,11 +1,4 @@
 package robot.hopper;
-
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkLowLevel.MotorType;
-
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import robot.Ports;
-
 public interface HopperIO {
   void setVoltage(double voltage);
   void getVoltage(double voltage);
