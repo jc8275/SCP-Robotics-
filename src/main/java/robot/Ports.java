21045782c22ca988d516b737f1df949374ee8da3
Joseph <jyf2025@gmail.com>
2025-05-15 16:41:17 -0400

    I am joseph too

--- src/main/java/robot/Ports.java
+++ src/main/java/robot/Ports.java
@@ -8,31 +8,26 @@ public final class Ports {
   }
 
   public static final class Intake {
-    public static final int INTAKE_MOTOR = 1; //This is for intake
+    public static final int INTAKE_MOTOR = 2; //This is for intake
 
   }
 
   public static final class Drive {
-    public static final int RIGHT_LEADER = 1;
-    public static final int RIGHT_FOLLOWER = 2;
-    public static final int LEFT_LEADER = 3;
-    public static final int LEFT_FOLLOWER = 4;
+    public static final int RIGHT_LEADER = 3;
+    public static final int RIGHT_FOLLOWER = 4;
+    public static final int LEFT_LEADER = 5;
+    public static final int LEFT_FOLLOWER = 6;
 
-    public static final int GYRO_CHANNEL = 1;
+    public static final int GYRO_CHANNEL = 7;
   }
 
   public static final class Shooter {
-    public static final int SHOOTER_ONE = 1; // replace with the id for the robot later 
+    public static final int SHOOTER_ONE = 8; // replace with the id for the robot later 
   }
 
-  
-    
-
-  
-
   public static final class Hopper {
-    public static final int HOPPER_TOP = 1;
-    public static final int HOPPER_BOTTOM = 2;
+    public static final int HOPPER_TOP = 9;
+    public static final int HOPPER_BOTTOM = 10;
   }
 }
 
